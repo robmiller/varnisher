a4b1af7444e65089a2203189c445dcbfa433ad2c
Rob Miller <rob@bigfish.co.uk>
2011-11-10 20:29:41 +0000

    Include some files missed in the last commit -- whoops.

---
+++ lib/domainpurger.rb
@@ -0,0 +1,25 @@
+require 'net/http'
+
+# This requires a special bit of VCL:
+#
+# if ( req.request == "DOMAINPURGE" ) {
+# 	if ( client.ip ~ auth ) {
+# 		ban("obj.http.x-host == " + req.http.host);
+# 		error 200 "Purged.";
+# 	}
+# }
+
+class DomainPurger
+  def initialize(domain)
+    s = TCPSocket.open(PROXY_HOSTNAME, PROXY_PORT)
+    s.print("DOMAINPURGE / HTTP/1.1\r\nHost: #{domain}\r\n\r\n")
+
+    if s.read =~ /HTTP\/1\.1 200 Purged\./
+      puts "Purged  #{domain}"
+    else
+      puts "Failed to purge #{domain}"
+    end
+
+    s.close
+  end
+end
---
+++ varnish.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+require 'optparse'
+require 'lib/pagepurger'
+require 'lib/domainpurger'
+
+$options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = 'Usage: varnish.rb [options] action target'
+
+  opts.on('-h', '--help', 'Display this help') do
+    puts opts
+  end
+
+  $options[:verbose] = false 
+  opts.on('-v', '--verbose', 'Output more information') do
+    $options[:verbose] = true
+  end
+
+  $options[:hostname] = 'localhost'
+  opts.on('-H', '--hostname HOSTNAME', 'Hostname/IP address of your Varnish server') do |hostname|
+    $options[:hostname] = hostname
+  end
+
+  $options[:port] = 80
+  opts.on('-p', '--port PORT', 'Port your Varnish server is listening on') do |port|
+    $options[:port] = port
+  end
+
+end
+
+optparse.parse!
+
+# All our libs use these constants.
+PROXY_HOSTNAME = $options[:hostname]
+PROXY_PORT = $options[:port].to_i
+
+if ( ARGV.length < 2 )
+  puts "You must specify both an action and a target."
+end
+
+action = ARGV[0]
+target = ARGV[1]
+
+case action
+  when "purge"
+    # If target is a valid URL, then assume we're purging a page and its contents.
+    if target =~ /^[a-z]+:\/\//
+      PagePurger.new target
+    end
+
+    # If target is a hostname, assume we want to purge an entire domain.
+    if target =~ /^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$/
+      DomainPurger.new target
+    end
+end
