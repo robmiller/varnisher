d83105a122f7b47f73da93549a96912ebcd41f28
Rob Miller <rob@bigfish.co.uk>
2013-08-16 21:15:09 +0100

    Refactor Page Purger
    - Remove logic from the constructor, so that it only, well, constructs.
      A call must be made to #purge to actually run the purging of that
      page.
    - Pull resource definitions out of #find_resources and into the class
      definition
    - Bring sanity to the line-endings in the log output; they went haywire
      after we switched to the proper logger
    - Massively improve the relative URI handling by using URI.join rather
      than my previous homegrown solution
    - Generally make things a bit more OO (e.g. making it so that we're not
      constantly passing URLs as messages but storing them on the instance
      instead

--- lib/varnisher/pagepurger.rb
+++ lib/varnisher/pagepurger.rb
@@ -6,6 +6,17 @@ require 'parallel'
 module Varnisher
   # Purges an individual URL from Varnish.
   class PagePurger
+    # A bash at an abstract representation of resources. All you need
+    # is an XPath, and what attribute to select from the matched
+    # elements.
+    Resource = Struct.new :name, :selector, :attribute
+    def self.resources
+      [
+        Resource.new('stylesheet', 'link[rel~=stylesheet]', 'href'),
+        Resource.new('JavaScript file', 'script[src]', 'src'),
+        Resource.new('image file', 'img[src]', 'src')
+      ]
+    end
 
     # Purges the given URL from the Varnish cache.
     #
@@ -18,42 +29,6 @@ module Varnisher
       @uri = URI.parse(url)
 
       @urls = []
-
-      # First, purge the URL itself; that means we'll get up-to-date
-      # references within that page.
-      Varnisher.log.info "Purging #{@url}...\n\n"
-      purge(@url)
-
-      # Then, do a fresh GET of the page and queue any resources we find on it.
-      Varnisher.log.debug "\n\n"
-      Varnisher.log.info "Looking for external resources on #{@url}..."
-
-      fetch_page(@url)
-
-      Varnisher.log.debug "\n"
-      Varnisher.log.info "#{@urls.length} total resources found.\n\n"
-
-      if @urls.length == 0
-        Varnisher.log.info 'No resources found. Abort!'
-        return
-      end
-
-      # Let's figure out which of these resources we can actually purge
-      # - whether they're on our server, etc.
-      Varnisher.log.info "Tidying resources...\n"
-      tidy_resources
-      Varnisher.log.info "#{@urls.length} purgeable resources found.\n\n"
-
-      # Now, purge all of the resources we just queued.
-      Varnisher.log.info 'Purging resources...'
-
-      Varnisher.log.debug "\n\n"
-
-      purge_queue
-
-      Varnisher.log.debug "\n"
-
-      Varnisher.log.info "Nothing more to do!\n\n"
     end
 
     # Sends a PURGE request to the Varnish server, asking it to purge
@@ -72,53 +47,54 @@ module Varnisher
     # More about purging can be found
     # [in the Varnish documentation][purging-and-banning].
     #
-    # [purging-and-banning]: https://www.varnish-cache.org/docs/3.0/tutorial/purging.html
+    # [purging-and-banning]: http://varnish-cache.org/docs/3.0/tutorial/purging.html
     #
     # @api private
-    def purge(url)
-      purged = Varnisher.purge(url)
+    def purge
+      Varnisher.log.info "Purging #{@url}..."
+
+      purged = Varnisher.purge(@url)
       if purged
-        Varnisher.log.debug "Purged #{url}"
+        Varnisher.log.info ''
+        Varnisher.log.debug "Purged #{@url}"
       else
-        Varnisher.log.info "Failed to purge #{url}"
+        Varnisher.log.info "Failed to purge #{@url}\n"
       end
+
+      purge_resources
+    end
+
+    # Purges all the resources on the given page.
+    def purge_resources
+      fetch_page
+
+      return if @urls.empty?
+
+      tidy_resources
+      purge_queue
     end
 
     # Fetches a page and parses out any external resources (e.g.
     # JavaScript files, images, CSS files) it finds on it.
     #
-    # @param url [String, URI]
-    #
     # @api private
-    def fetch_page(url)
+    def fetch_page
+      Varnisher.log.info "Looking for external resources on #{@url}..."
+
       begin
-        uri = URI.parse(URI.encode(url.to_s.strip))
+        @doc = Nokogiri::HTML(Net::HTTP.get_response(@uri).body)
       rescue
-        Varnisher.log.info "Couldn't parse URL for resource-searching: #{url}"
+        Varnisher.log.info "Hmm, I couldn't fetch that URL. Sure it's right?\n"
         return
       end
 
-      headers = {
-        'User-Agent'     => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.106 Safari/535.2',
-        'Accept-Charset' => 'utf-8',
-        'Accept'         => 'text/html'
-      }
+      @urls = find_resources
 
-      begin
-        doc = Nokogiri::HTML(Net::HTTP.get_response(uri).body)
-      rescue
-        Varnisher.log.info "Hmm, I couldn't seem to fetch that URL. Sure it's right?\n"
-        return
-      end
-
-      find_resources(doc) do |resource|
-        Varnisher.log.debug "Found #{resource}"
-        queue_resource(resource)
-      end
+      Varnisher.log.debug ''
+      Varnisher.log.info "#{@urls.length} total resources found.\n"
     end
 
-    # Given a Nokogiri document, will return an array of the resources
-    # within that document.
+    # Returns an array of resources contained within the current page.
     #
     # Resources include things like CSS files, images, and JavaScript
     # files.
@@ -126,44 +102,24 @@ module Varnisher
     # If a block is given, the block will be executed once for each
     # resource.
     #
-    # @param doc A Nokogiri document
-    #
     # @return [Array] An array of strings, each representing a URL
     #
     # @api private
-    def find_resources(doc)
-      return unless doc.respond_to? 'xpath'
+    def find_resources
+      found = []
 
-      # A bash at an abstract representation of resources. All you need
-      # is an XPath, and what attribute to select from the matched
-      # elements.
-      res = Struct.new :name, :selector, :attribute
-      res_defs = [
-        res.new('stylesheet', 'link[rel~=stylesheet]', 'href'),
-        res.new('JavaScript file', 'script[src]', 'src'),
-        res.new('image file', 'img[src]', 'src')
-      ]
+      self.class.resources.each do |res|
+        @doc.css(res.selector).each do |e|
+          attribute = e[res.attribute]
 
-      resources = []
+          Varnisher.log.debug("Found resource: #{attribute}")
 
-      res_defs.each do |resource|
-        doc.css(resource.selector).each do |e|
-          att = e[resource.attribute]
-          yield att if block_given?
-          resources << att
+          yield attribute if block_given?
+          found << attribute
         end
       end
 
-      resources
-    end
-
-    # Adds a URL to the processing queue.
-    #
-    # @param url [String]
-    #
-    # @api private
-    def queue_resource(url)
-      @urls << url.to_s
+      found
     end
 
     # Tidies up the resource queue, converting relative URLs to
@@ -173,41 +129,12 @@ module Varnisher
     #
     # @api private
     def tidy_resources
-      valid_urls = []
+      Varnisher.log.info 'Tidying resources...'
 
-      @urls.each do |url|
+      @urls = @urls.map { |url| URI.join(@uri, url) }
+        .select { |uri| uri.scheme == 'http' && uri.host == @uri.host }
 
-        # If we're dealing with a host-relative URL (e.g. <img
-        # src="/foo/bar.jpg">), absolutify it.
-        url.match(/\//) do
-          url = @uri.scheme + '://' + @uri.host + url.to_s
-        end
-
-        # If we're dealing with a path-relative URL, make it relative to
-        # the current directory.
-        unless url.to_s =~ %r([a-z]+://)
-
-          # Take everything up to the final / in the path to be the
-          # current directory.
-          @uri.path.match(/^(.*)\//) do |m|
-            url = @uri.scheme + '://' + @uri.host + m[1] + '/' + url.to_s
-          end
-        end
-
-        begin
-          uri = URI.parse(url)
-        rescue
-          next
-        end
-
-        # Skip URLs that aren't HTTP, or that are on different domains.
-        next if uri.scheme != 'http'
-        next if uri.host != @uri.host
-
-        valid_urls << url
-      end
-
-      @urls = valid_urls.dup
+      Varnisher.log.info "#{@urls.length} purgeable resources found.\n"
     end
 
     # Processes the queue of URLs, sending a purge request for each of
@@ -215,11 +142,15 @@ module Varnisher
     #
     # @api private
     def purge_queue
+      Varnisher.log.info 'Purging resources...'
+
       Parallel.map(@urls) do |url|
         Varnisher.log.debug "Purging #{url}..."
 
-        purge(url)
+        Varnisher.purge(url.to_s)
       end
+
+      Varnisher.log.info 'Done.'
     end
 
   end
